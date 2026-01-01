e8356ef520395d9aa0addb07f1792c05346c4fe6
Ganesh Sunkara <ganeshsunkra90@gmail.com>
2026-01-01 17:18:58 +0530

    Fix CSS: Use CompressedStaticFilesStorage for reliable serving

--- pustakam/settings.py
+++ pustakam/settings.py
@@ -158,7 +158,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
